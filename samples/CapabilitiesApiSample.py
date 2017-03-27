0a2fe39ece749c8e33ea67054aaacc096640472d
Roman Saul <roman.saul@leanix.net>
2017-03-27 12:02:02 +0200

    [#142030721] Artem fixed list deserialisation

--- samples/CapabilitiesApiSample.py
+++ samples/CapabilitiesApiSample.py
@@ -1,23 +1,18 @@
+#!/usr/bin/env python
+
 import sys
+import json
 
 sys.path = ['../src'] + sys.path
-
 from leanix import *
 
 if __name__ == "__main__":
-    client = swagger.ApiClient(apiToken='LVdc8Ns95w45x4SxLxcnxw5EawxdueqYW3b6wHXL', tokenProviderHost='test-svc.leanix.net', basePath='https://test-app.leanix.net/artemdemo/api/v1')
+    client = swagger.ApiClient(apiToken='6x4Kt6QXX4WxfvWOjG4cUnq9Vr3AH6DOkNgTfBgw', tokenProviderHost='svc.leanix.net', basePath='https://app.leanix.net/demo/api/v1')
     capabilitiesApi = BusinessCapabilitiesApi.BusinessCapabilitiesApi(client)
 
     capabilities = capabilitiesApi.getBusinessCapabilities(relations='true')
 
     for bc in capabilities:
-        if bc.factSheetHasChildren is not None:
-            for ch in bc.factSheetHasChildren:
-                print ch
-
-    for bc in capabilities:
-        print 'Children of ' + bc.name + ':\n'
-        if bc.factSheetHasChildren is not None:
-            # This is a dead path even if the response contains children. The deserialization somehow breaks in line swagger.py:163 after first recursive call (objClass = "BusinessCapability")
-            for child in bc.factSheetHasChildren:
-                print '> ' + child + '\n'
+        print 'Children of ' + bc.name + ':'
+        for child in bc.factSheetHasChildren:
+            print '> ' + json.dumps(vars(child))
